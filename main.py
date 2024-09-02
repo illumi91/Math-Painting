a342ec87d39d5904b5ea6edd48312792f4936f3e
luigi <luigi.fiori.lf0303@gmail.com>
2024-09-02 08:36:29 +0100

    WIP

---
+++ main.py
@@ -0,0 +1,104 @@
+import os
+import webbrowser
+import numpy as np
+from PIL import Image
+
+class Canvas:
+    """
+    Defines the canvas dimension and colour (black or white).
+    """
+    def __init__(self, width, height, colour):
+        self.width = width
+        self.height = height
+        self.colour = colour
+        
+    def draw(self):
+        canvas = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        if self.colour == "black": 
+            canvas[:] = [0, 0, 0]
+        else:
+            canvas[:] = [255, 255, 255]
+        return canvas
+
+
+class Rectangle:
+    """
+    Rectangle shape defined given x and y coordinates, 
+    dimensions and colour in RGB format.
+    """
+    def __init__(self, x, y, width, height, red, green, blue):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.red = red
+        self.green = green
+        self.blue = blue
+    
+    def draw(self, canvas):
+        canvas[self.y:self.y + self.height, self.x:self.x + self.width] = [self.red, self.green, self.blue]
+        pillow_image = Image.fromarray(canvas, "RGB")
+        
+        return canvas, pillow_image
+
+
+class Square(Rectangle):
+    """
+    Square shape which is a particular rectangle.
+    """
+    def __init__(self, x, y, width, red, green, blue):
+        super().__init__(x, y, width, width, red, green, blue)
+        
+        
+class SaveImage:
+    def __init__(self, filename):
+        self.filename = filename
+        
+    def save(self, pillow_image):
+        
+        pillow_image.save(f"files/{self.filename}.png")
+        
+        webbrowser.open("file://" + os.path.realpath(f"files/{self.filename}"))
+
+
+canvas_width = int(input("Enter canvas width: "))
+canvas_height = int(input("Enter canvas height: "))
+canvas_colour = input("Enter canvas colour (black or white): ")
+canvas = Canvas(canvas_width, canvas_height, canvas_colour)
+canvas = canvas.draw()
+
+running = True
+
+while running:
+    shape_to_draw = input("What shape would you like to draw (square or rectangle)? Enter quit to exit when you are done. :) ")
+    if shape_to_draw == "quit":
+        
+        filename = input("Enter filename for the image: ")
+        save_image = SaveImage(filename)
+        save_image.save(pillow_image)
+        
+        running = False
+        
+        continue
+    
+    if shape_to_draw == 'square':
+        square_x = int(input("Enter x of the square: "))
+        square_y = int(input("Enter y of the square: "))
+        square_width = int(input("Enter width of the square: "))
+        square_red = int(input("How much red should the square have? "))
+        square_green = int(input("How much green should the square have? "))
+        square_blue = int(input("How much blue should the square have? "))
+        square = Square(square_x, square_y, square_width, red=255, green=155, blue=0)
+        canvas, pillow_image = square.draw(canvas)
+        
+    else:
+        rectangle_x = int(input("Enter x of the rectangle: "))
+        rectangle_y = int(input("Enter y of the rectangle: "))
+        rectangle_width = int(input("Enter width of the rectangle: "))
+        rectangle_height = int(input("Enter height of the rectangle: "))
+        rectangle_red = int(input("How much red should the rectangle have? "))
+        rectangle_green = int(input("How much green should the rectangle have? "))
+        rectangle_blue = int(input("How much blue should the rectangle have? "))
+        rectangle = Rectangle(rectangle_x, rectangle_y, rectangle_width, rectangle_height, rectangle_red, rectangle_green, rectangle_blue)
+        canvas, pillow_image = rectangle.draw(canvas)
+
